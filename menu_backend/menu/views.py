229ac261b9885a6bcf557e0c35bfb6186ce35f0f
Jim Galvan <jimmy-galvan@live.com.mx>
2024-05-10 19:20:08 -0700

    add endpoint to get categories list by menu id

--- menu_backend/menu/views.py
+++ menu_backend/menu/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets, permissions
 
 from menu.permissions import IsOwnerOrReadOnly
-from menu.models import  Menu, Category, MenuItem
+from menu.models import Menu, Category, MenuItem
 from menu.serializers import UserSerializer, MenuSerializer, CategorySerializer, \
     MenuItemSerializer
 
@@ -15,12 +15,30 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     serializer_class = UserSerializer
 
 
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
+
 class MenuViewSet(viewsets.ModelViewSet):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+    @action(detail=True, methods=['get'])
+    def menu_items(self, request, pk=None):
+        menu = self.get_object()
+        menu_items = MenuItem.objects.filter(menu=menu)
+        serializer = MenuItemSerializer(menu_items, many=True, context={'request': request})
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'])
+    def categories(self, request, pk=None):
+        menu = self.get_object()
+        categories = Category.objects.filter(menu=menu)
+        serializer = CategorySerializer(categories, many=True, context={'request': request})
+        return Response(serializer.data)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
